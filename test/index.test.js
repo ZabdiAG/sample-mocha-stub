a3a3cdf8505d958854c28eec383caa931f15b375
Luis Aguilera <aguilera.glz.luis@gmail.com>
2017-02-25 16:39:28 -0600

    finished test with stub promise

--- test/index.test.js
+++ test/index.test.js
@@ -3,13 +3,23 @@
 const assert = require('assert')
 const Stubbing = require('../index')
 const sinon = require('sinon')
+const Promise = require('bluebird')
 
 describe('Class Stubbing', () => {
   describe('run', () => {
     it('should return no', done => {
       let stubbing = new Stubbing()
       // TODO: stub _mochThisOne method
-      stubbing.run((val) => { console.log(val) })
+      sinon.stub(stubbing, '_mochThisOne', () => {
+        return new Promise((resolve, reject) => {
+          resolve(this.affected = 'no')
+        })
+      })
+
+      stubbing.run((val) => {
+        assert(this.affected === 'no')
+        done()
+      })
     }).timeout(5000)
   })
 })
